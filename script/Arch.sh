540a767db2efe234dcf12d875a396a18d57d2d04
Artic42 <main@artic42.com>
2020-03-27 00:03:49 +0100

    First complete version

--- script/Arch.sh
+++ script/Arch.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 #Update the entire system
-sudo pacman -Syu
+yes | sudo pacman -Syu
 
 #Install basic programs
-sudo pacman -S git gcc gdb vim python3 python3-pip neofetch ctags tmux make
-sudo pip3-install powerline-shell
+yes | sudo pacman -S git gcc gdb vim python3 python3-pip neofetch ctags tmux make
+yes | sudo pip3-install powerline-shell
 
 #Execute non-distro dependeding part
-source ~/.script/copy_conf.sh
+source ~/DotFiles/script/copy_conf.sh
 
 #Last order is to reboot
 sudo reboot
