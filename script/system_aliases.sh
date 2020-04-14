2180f778e63efa03c1facf2d07aade1daeacf79e
Artic42 <main@artic42.com>
2020-04-14 16:30:19 +0200

    Correcion en aliases tmux

--- script/system_aliases.sh
+++ script/system_aliases.sh
@@ -6,13 +6,20 @@ alias la='ls -la'
 alias l='ls -CF'
 
 #Aliases for launching applications
-alias vi='vim'
-alias tmuxs='tmux new -s'
+	alias vi='vim'
+	#Tmux Shortcuts
+	alias tmuxs='tmux new -s'
+	alias tmuxa='tmux attach'
+	alias tmuxd='tmux detach'
 	#Git shortcuts
 	alias gcl='git clone'
 	alias gco='git add -A && git commit -m'
 	alias gps='git push'
 	alias gpl='git pull'
+	#Python aliases
+	alias python='python3'
+	alias py='python3'
+
 
 #Shutdown and reboot
 alias sdn='sudo shutdown now'
