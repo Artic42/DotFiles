87525902598305f0e880cb0dfe0c3394014711ed
Artic42 <main@artic42.com>
2020-05-12 17:44:49 +0200

    Añadir Fedora

--- script/Fedora.sh
+++ script/Fedora.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #Update the entire system
-yes | sudo pacman -Syu
+yes | sudo dnf upgrade
 
 #Install basic programs
-yes | sudo pacman -S git gcc gdb vim python3 python3-pip neofetch ctags tmux make rxvt-unicode
-yes | sudo pip3-install powerline-shell
+yes | sudo dnf install git gcc gdb vim python3 python3-pip neofetch exuberant-ctags tmux make rxvt-unicode
+yes | sudo pip3 install powerline-shell
 
 #Clone DotFiles
 cd ~
 git clone https://github.com/artic42/DotFiles.git
 
 #Execute non-distro dependeding part
-~/DotFiles/script/copy_conf.sh
+sh ~/DotFiles/script/copy_conf.sh
 
 #Last order is to reboot
 ~/.script/prompt_reboot.sh
