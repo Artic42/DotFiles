39e896f5c303689552fc409863caa686ff0bca63
Artic42 <main@artic42.com>
2022-01-05 17:08:31 +0000

    Cleaning

--- bin/python/lib/modifyFile.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def replaceString (path, old, new):
-    command = 'sed -i "s/' + old + '/' + new + '/" ' + path
-    os.system (command)
-
-def copyFile (referencePath, destPath):
-    command = "cp -f " + referencePath + " " + destPath
-    os.system (command)
